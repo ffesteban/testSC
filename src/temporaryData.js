2de3e631d5a89ce6b2880be82541e4fd4ef824d8
FF_EstebanValverde <esteban@firstfactory.com>
2018-02-14 16:46:35 -0600

    Commit04

--- src/temporaryData.js
+++ src/temporaryData.js
@@ -2,61 +2,61 @@ let shopData = [
     {
         Id: 1,
         Description: 'Shirt Quiksilver',
-        Image: 'images/Men/M01.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M01.jpg',
         Price: 25
     },
     {
         Id: 2,
         Description: 'Hoddie Volcom',
-        Image: 'images/Men/M02.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M02.jpg',
         Price: 48
     },
     {
         Id: 3,
         Description: 'Shirt Billabong',
-        Image: 'images/Men/M03.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M03.jpg',
         Price: 22
     },
     {
         Id: 4,
         Description: 'T-Shirt Hurley',
-        Image: 'images/Men/M04.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M04.jpg',
         Price: 17
     },
     {
         Id: 5,
         Description: 'Billabong Hoddie',
-        Image: 'images/Men/M05.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M05.jpg',
         Price: 21
     },
     {
         Id: 6,
         Description: 'Hoddie Volcom',
-        Image: 'images/Men/M06.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M06.jpg',
         Price: 32
     },
     {
         Id: 7,
         Description: 'Hoddie Hurley',
-        Image: 'images/Men/M07.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M07.jpg',
         Price: 35
     },
     {
         Id: 8,
         Description: 'T-Shirt Volcom',
-        Image: 'images/Men/M08.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M08.jpg',
         Price: 12
     },
     {
         Id: 9,
         Description: 'T-Shirt Nasa',
-        Image: 'images/Men/M09.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M09.jpg',
         Price: 14
     },
     {
         Id: 10,
         Description: 'T-Shirt Sacata',
-        Image: 'images/Men/M10.jpg',
+        Image: 'https://raw.githubusercontent.com/ffesteban/testSC/master/public/images/Men/M10.jpg',
         Price: 18
     }
 ];
